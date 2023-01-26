4e82160938b84a2dbc4ee9f4dbd4de31ff13297c
FlorianKomnino <florian.komnino@outlook.it>
2023-01-26 15:21:30 +0100

    setup repo

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+</head>
+
+<body>
+    <main>
+        <form action="" method="GET">
+            <input type="text" name="numberOfCharacters">
+            <button>Generate password</button>
+
+
+        </form>
+    </main>
+</body>
+
+</html>
