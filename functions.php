33521c08275c82afaf6d38dabfaf80dac7b63703
FlorianKomnino <florian.komnino@outlook.it>
2023-01-26 19:05:19 +0100

    completed exercise with bonuses, no styling

--- functions.php
+++ functions.php
@@ -2,14 +2,32 @@
 session_start();
 
 
+
+
 function getRandomNumber($min, $max)
 {
     return random_int($min, $max);
 }
 
+
+
 if (isset($_GET['numberOfCharacters']) && preg_match($number_validation_regex, $_GET['numberOfCharacters'])) {
-    while (strlen($generatedPassword) <  $_GET['numberOfCharacters']) {
-        $generatedPassword = $generatedPassword . "{$characters[(getRandomNumber(0,$charactersLength))]}";
+
+    if (isset($_GET['lettersChoise']) || isset($_GET['numbersChoise']) || isset($_GET['symbolsChoise'])) {
+
+        $categoriesChosenAddedString =
+            (isset($_GET['lettersChoise']) ? $onlyLetters : "") .
+            (isset($_GET['numbersChoise']) ? $onlyNumbers : "") .
+            (isset($_GET['symbolsChoise']) ? $onlySymbols : "");
+
+        while (strlen($generatedPassword) <  $_GET['numberOfCharacters']) {
+            $generatedPassword = $generatedPassword . "{$categoriesChosenAddedString[(getRandomNumber(0,$charactersLength))]}";
+        }
+    } else {
+
+        while (strlen($generatedPassword) <  $_GET['numberOfCharacters']) {
+            $generatedPassword = $generatedPassword . "{$characters[(getRandomNumber(0,$charactersLength))]}";
+        }
     }
     $_SESSION['generatedPasswordInDb'] = $generatedPassword;
 } elseif (isset($_GET['numberOfCharacters']) && !preg_match($number_validation_regex, $_GET['numberOfCharacters'])) {
--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 session_start();
 
 $number_validation_regex = "/^\\d+$/";
-$characters = "!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,/';#][=-?><~@:}{+_)(*&^%1234567890";
+$onlyLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$onlySymbols = "!.,/';#][=-?><~@:}{+_)(*&^%";
+$onlyNumbers = "1234567890";
+$characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,/';#][=-?><~@:}{+_)(*&^%1234567890";
 $generatedPassword = "";
 $charactersLength = strlen($characters);
 ?>
@@ -22,8 +25,23 @@ $charactersLength = strlen($characters);
 <body>
     <main>
         <form action="" method="GET">
+            <h1>Generazione standard</h1>
+            <p>per la generazione della tua password verrano utilizzati numeri, simboli e lettere in modo randomico.</p>
+
             <input type="text" name="numberOfCharacters">
             <button>Generate password</button>
+            <br>
+            <h1>Selezione avanzata</h1>
+            <p>Seleziona la tipologia di caratteri che vuoi sia utilizzata nella tua password</p>
+            <p>Puoi selezionare piu' di una categoria</p>
+            <input type="checkbox" name="lettersChoise" id="lettersChoise">
+            <label for="lettersChoise">Selezione "lettere"</label>
+            <br>
+            <input type="checkbox" name="numbersChoise" id="numbersChoise">
+            <label for="numbersChoise">Selezione "numeri"</label>
+            <br>
+            <input type="checkbox" name="symbolsChoise" id="symbolsChoise">
+            <label for="symbolsChoise">Selezione "simboli"</label>
         </form>
 
         <h1>
